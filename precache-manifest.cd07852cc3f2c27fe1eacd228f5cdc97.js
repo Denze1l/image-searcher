92fc57ca4d70793339f31bb9858a3654448d54e9
Denze1l <dvornikovd.o.1998@gmail.com>
2019-10-15 14:18:51 +0300

    Updates

--- precache-manifest.cd07852cc3f2c27fe1eacd228f5cdc97.js
+++ precache-manifest.cd07852cc3f2c27fe1eacd228f5cdc97.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ecc1d10b9a013802456ad3d4c0f7edb5",
+    "revision": "990e12ae3e94bffe731248e0764b96c5",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
-    "revision": "5022b54fefc53f409bfe",
-    "url": "/goit-react-hw-03-image-finder/static/css/main.b7780582.chunk.css"
+    "revision": "4ed5ff45bf63e74098ce",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.31328abf.chunk.css"
   },
   {
     "revision": "470ce9219e126a46da35",
     "url": "/goit-react-hw-03-image-finder/static/js/2.00c9107c.chunk.js"
   },
   {
-    "revision": "5022b54fefc53f409bfe",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.6bd46086.chunk.js"
+    "revision": "4ed5ff45bf63e74098ce",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f2deaa4a.chunk.js"
   },
   {
     "revision": "716f83227a277155b007",
